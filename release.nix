c37e16706e21d8f61bd0113182825d5cc6159927
Dan Svoboda <dan@dansvo.com>
2019-09-14 20:42:20 -0400

    initial setup of keyboard  build

---
+++ release.nix
@@ -0,0 +1,19 @@
+let
+  pkgs = import <nixpkgs> { };
+  stdenv = pkgs.stdenv;
+  split_ergo_board = pkgs.haskellPackages.callPackage ./split-ergo-board.nix { };
+
+in
+  {
+  scad_files = pkgs.stdenv.mkDerivation {
+    name = "kbd_scad_files";
+    inherit split_ergo_board;
+    builder = builtins.toFile "builder.sh"
+      ''
+        source $stdenv/setup
+        mkdir $out
+        cd $out
+        $split_ergo_board/bin/split_ergo_board
+      '';
+  };
+  }
